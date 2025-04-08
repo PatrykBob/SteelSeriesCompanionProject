564c95bfe007b57853722fe8d0ca5072fe030ea0
PatrykSajdok <patryk.sajdok@4experience.co>
2025-04-08 20:02:50 +0200

    Added ExternalCommunicationShared project with const provider

---
+++ SteelSeriesSonarCompanion/ExternalCommunicationShared/ExternalCommunicationConstProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace SteelSeriesCompanion.ExternalCommunication.Shared
+{
+	public static class ExternalCommunicationConstProvider
+	{
+		public const string SERVER_SEARCH_REQUEST = "DISCOVER_SERVER";
+
+		public const int BROADCAST_PORT = 8888;
+	}
+}

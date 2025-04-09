c7fb980993660e6c8a10c3b33fcec81600fc0ae5
PatrykSajdok <patryk.sajdok@4experience.co>
2025-04-09 23:05:46 +0200

    Added SonarSetupLoader with SonartSetupConfig

---
+++ SteelSeriesSonarCompanion/SteelSeriesCompanionCoreApp/Setup/SonarSetupConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace SteelSeriesCompanionCoreApp.Setup
+{
+	public class SonarSetupConfig
+	{
+		public string? encryptedAddress;
+		public string? ggEncryptedAddress;
+		public string? address;
+	}
+}
---
+++ SteelSeriesSonarCompanion/SteelSeriesCompanionCoreApp/Setup/SonarSetupLoader.cs
@@ -0,0 +1,38 @@
+﻿using SteelSeriesCompanion.SharedCore.Converters;
+using System.IO;
+
+namespace SteelSeriesCompanionCoreApp.Setup
+{
+	public static class SonarSetupLoader
+	{
+		private const string SONAR_SETUP_FILE_PATH = "C:\\ProgramData\\SteelSeries\\GG\\coreProps.json";
+		private const char ADDRESS_SPLIT_CHARACTER = ':';
+
+		public static bool TryGetSonarSetupPort (out int setupPort)
+		{
+			setupPort = default;
+
+			if (File.Exists(SONAR_SETUP_FILE_PATH) == false)
+			{
+				return false;
+			}
+
+			string json = File.ReadAllText(SONAR_SETUP_FILE_PATH);
+			SonarSetupConfig? config = JsonConverter.ConvertFromJSON<SonarSetupConfig>(json);
+
+			if (config == null || string.IsNullOrEmpty(config.ggEncryptedAddress) == true)
+			{
+				return false;
+			}
+
+			string[] addressPartCollection = config.ggEncryptedAddress.Split(ADDRESS_SPLIT_CHARACTER);
+
+			if (int.TryParse(addressPartCollection[^1], out setupPort) == false)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}

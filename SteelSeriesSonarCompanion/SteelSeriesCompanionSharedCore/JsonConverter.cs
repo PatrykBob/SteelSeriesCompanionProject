16ac5c1638927f36482cea4603999f42a42cff0d
PatrykSajdok <patryk.sajdok@4experience.co>
2025-04-09 22:17:03 +0200

    Added JsonConverter to shared core

---
+++ SteelSeriesSonarCompanion/SteelSeriesCompanionSharedCore/JsonConverter.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SteelSeriesCompanion.SharedCore.Converters
+{
+	public static class JsonConverter
+	{
+		public static T? ConvertFromJSON<T> (string json)
+		{
+			return JsonConvert.DeserializeObject<T>(json);
+		}
+	}
+}

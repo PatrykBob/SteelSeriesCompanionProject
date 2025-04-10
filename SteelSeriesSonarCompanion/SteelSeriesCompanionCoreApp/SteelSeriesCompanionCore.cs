c2237abe8203b0421f3c950874a24ed972e3b375
Bobu <patryk.xperia.s@gmail.com>
2025-04-10 17:31:37 +0200

    Added core class

---
+++ SteelSeriesSonarCompanion/SteelSeriesCompanionCoreApp/SteelSeriesCompanionCore.cs
@@ -0,0 +1,32 @@
+﻿using SteelSeriesCompanion.Extension;
+using SteelSeriesCompanion.SharedCore;
+using SteelSeriesCompanionCoreApp.Setup;
+using SteelSeriesSonarCompanion.Communication.Internal;
+
+namespace SteelSeriesCompanion
+{
+	public class SteelSeriesCompanionCore : ISteelSeriesCompanionCore
+	{
+		private InternalCommunicationFacade InternalFacade { get; set; } = new();
+		private SteelSeriesCompanionExtensionController ExtensionController { get; set; } = new();
+
+		public async Task SetChannelVolume (SoundChannel channel, float volume)
+		{
+			await InternalFacade.SetChannelVolume(channel.ToString(), volume);
+		}
+
+		public async void Initialize ()
+		{
+			if (SonarSetupLoader.TryGetSonarSetupPort(out int setupPort))
+			{
+				await InternalFacade.Initialize(setupPort);
+			}
+			else
+			{
+				// TODO inform user about problem
+			}
+
+			ExtensionController.Initialize(this);
+		}
+	}
+}

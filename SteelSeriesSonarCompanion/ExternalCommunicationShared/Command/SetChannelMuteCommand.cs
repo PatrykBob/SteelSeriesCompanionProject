f97c6470cc0838e68354230d1e60fd1f81cbdded
Bobu <patryk.xperia.s@gmail.com>
2025-04-17 20:01:12 +0200

    Added public fields for serialization

--- SteelSeriesSonarCompanion/ExternalCommunicationShared/Command/SetChannelMuteCommand.cs
+++ SteelSeriesSonarCompanion/ExternalCommunicationShared/Command/SetChannelMuteCommand.cs
@@ -4,6 +4,9 @@ namespace SteelSeriesCompanion.ExternalCommunication.Shared.Command
 {
 	public class SetChannelMuteCommand (SoundChannel channel, bool mute) : BaseExternalCommunicationCommand(COMMAND_NAME)
 	{
+		public SoundChannel channel = channel;
+		public bool mute = mute;
+
 		public const string COMMAND_NAME = "SetMute";
 
 		public override void ExecuteCommand (ISteelSeriesCompanionCore core)

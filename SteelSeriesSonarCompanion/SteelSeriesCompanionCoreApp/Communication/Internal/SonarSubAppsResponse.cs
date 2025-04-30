530374f15b625ced0a73df63dc657a2bba705c67
Bobu <patryk.xperia.s@gmail.com>
2025-04-30 22:30:09 +0200

    Removed unnecessary using instructions

--- SteelSeriesSonarCompanion/SteelSeriesCompanionCoreApp/Communication/Internal/SonarSubAppsResponse.cs
+++ SteelSeriesSonarCompanion/SteelSeriesCompanionCoreApp/Communication/Internal/SonarSubAppsResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SteelSeriesSonarCompanion.Communication.Internal
+﻿namespace SteelSeriesSonarCompanion.Communication.Internal
 {
 	public class SonarSubAppsResponse
 	{

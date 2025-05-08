118a42d999fad0565647cb5d256ae38236e57ee4
Bobu <patryk.xperia.s@gmail.com>
2025-05-08 22:35:10 +0200

    Added external communication events

---
+++ SteelSeriesSonarCompanion/ExternalCommunicationShared/Event/BaseExternalCommunicationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace SteelSeriesCompanion.ExternalCommunication.Shared.Event
+{
+	public class BaseExternalCommunicationEvent
+	{
+		public string EventName { get; }
+
+		public BaseExternalCommunicationEvent (string eventName)
+		{
+			EventName = eventName;
+		}
+	}
+}
---
+++ SteelSeriesSonarCompanion/ExternalCommunicationShared/Event/VolumeSetupEvent.cs
@@ -0,0 +1,16 @@
+﻿using SteelSeriesCompanion.SharedCore;
+
+namespace SteelSeriesCompanion.ExternalCommunication.Shared.Event
+{
+	public class VolumeSetupEvent : BaseExternalCommunicationEvent
+	{
+		public List<VolumeData> VolumeDataCollection { get; } = new();
+
+		public const string EVENT_NAME = "VolumeSetup";
+
+		public VolumeSetupEvent (List<VolumeData> volumeDataCollection) : base(EVENT_NAME)
+		{
+			VolumeDataCollection = volumeDataCollection;
+		}
+	}
+}
